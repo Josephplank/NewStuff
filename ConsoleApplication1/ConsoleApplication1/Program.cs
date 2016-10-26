9530c8793d0b9610acdd3874b9bcaac59fe300f4
mikeplaank <mikeplaank@MIKESPC>
2016-10-26 10:17:52 -0500

    New

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            WriteLine("Hello world!");
+        }
+    }
+}
